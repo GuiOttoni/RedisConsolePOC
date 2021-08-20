fec52204b913dd1f6190c1557ff1ace36e0c7bd4
Guilherme Ottoni Escarelli De Alencar <guiottonie@gmail.com>
2021-08-19 23:06:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisConsolePOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("40.65.216.220");
+            var sub = redis.GetSubscriber();
+
+            var db = redis.GetDatabase();
+
+
+            sub.Subscribe("perguntas").OnMessage(
+            c =>
+            {
+                if (c.Message.ToString().Contains(":"))
+                {
+                    var dados = c.Message.ToString().Split(':');
+                    var chavePergunta = dados[0];
+                    var pergunta = dados[1];
+
+                    var value = Convert.ToInt32(chavePergunta.Substring(1));
+                    var resp = value * 2;
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine(resp);
+                    db.HashSet(chavePergunta, "Grupo01", resp);
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
